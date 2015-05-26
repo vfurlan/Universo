d993a069ed29a5a0ccb2cb31ddf6cd4c97730a1c
vfurlan <valentino_furlan@yahoo.it>
2015-05-26 11:44:58 +0200

    commentato index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,4 @@
-
+//converti i pixels in dpi
 function pixelsToDp(pixels){
     if ( Ti.Platform.displayCaps.dpi > 160 )
         return (pixels / (Ti.Platform.displayCaps.dpi / 160));
@@ -6,6 +6,7 @@ function pixelsToDp(pixels){
         return pixels;
 }
 
+//converte i dpi in pixels
 function dpToPixels(dp){
     if ( Ti.Platform.displayCaps.dpi > 160 )
           return (dp * (Ti.Platform.displayCaps.dpi / 160));
@@ -13,28 +14,34 @@ function dpToPixels(dp){
         return dp;
 }
 
-var dimMenu=Ti.Platform.displayCaps.dpi*0.7;
-var dimHalfMenu=dimMenu/2;
-var minDimMenu=Ti.Platform.displayCaps.dpi*0.1;
+//imposto le dimensioni del menu
+var dimMonitor=Ti.Platform.displayCaps.dpi;
+var dimMenu=dimMonitor*0.7;
+var dimHalfMenu=dimMonitor*0.35;
+var minDimMenu=dimMonitor*0.1;
 
+//animazione per spostare il menu verso destra
 var animateRight = Ti.UI.createAnimation({
 	left : dimMenu,
 	curve : Ti.UI.ANIMATION_CURVE_EASE_OUT,
 	duration : 300
 });
 
+//animazione per chiudere il menu
 var animateReset = Ti.UI.createAnimation({
 	left : 0,
 	curve : Ti.UI.ANIMATION_CURVE_EASE_OUT,
 	duration : 300
 });
 
+//animazione per spostare il menu verso sinistra
 var animateLeft = Ti.UI.createAnimation({
 	left : -dimMenu,
 	curve : Ti.UI.ANIMATION_CURVE_EASE_OUT,
 	duration : 300
 });
 
+//imposto variabili per la gestione del menu
 var touchStartX = 0;
 var touchRightStarted = false;
 var touchLeftStarted = false;
@@ -42,6 +49,7 @@ var buttonPressed = false;
 var hasSlided = false;
 var direction = "reset";
 
+//rileva la posizione del dito quando si tocca lo schermo
 $.movableview.addEventListener('touchstart', function(e) {
 	if (Ti.Platform.osname === 'android'){
 		touchStartX = pixelsToDp(e.x);
@@ -51,6 +59,7 @@ $.movableview.addEventListener('touchstart', function(e) {
 	}
 });
 
+//gestice lo slide del menu quando il dito viene alzato dallo schermo
 $.movableview.addEventListener('touchend', function(e) {
 	if (buttonPressed) {
 		buttonPressed = false;
@@ -78,6 +87,7 @@ $.movableview.addEventListener('touchend', function(e) {
 	touchLeftStarted = false;
 });
 
+//gestice lo slide del menu mentre il dito non viene alzato dallo schermo
 $.movableview.addEventListener('touchmove', function(e) {
 	var coords = $.movableview.convertPointToView({
 		x : e.x,
@@ -95,14 +105,14 @@ $.movableview.addEventListener('touchmove', function(e) {
 			(newLeft < 0 && newLeft >= -dimMenu)) {
 			$.movableview.left = newLeft;
 		
-			if (newLeft > 0 && !touchLeftStarted && !touchRightStarted) {
+			if (newLeft > 5 && !touchLeftStarted && !touchRightStarted) {
 				touchRightStarted = true;
 				Ti.App.fireEvent("sliderToggled", {
 					hasSlided : false,
 					direction : "right"
 				});
 			}
-			else if (newLeft < 0 && !touchRightStarted && !touchLeftStarted) {
+			else if (newLeft < -5 && !touchRightStarted && !touchLeftStarted) {
 				touchLeftStarted = true;
 				Ti.App.fireEvent("sliderToggled", {
 					hasSlided : false,
@@ -114,49 +124,7 @@ $.movableview.addEventListener('touchmove', function(e) {
 	
 });
 
-/*
-$.movableview.addEventListener('touchmove', function(e) {
-	var coords = $.movableview.convertPointToView({
-		x : e.x,
-		y : e.y
-	}, $.containerview);
-	var newLeft = coords.x - touchStartX;
-	if(touchStartX<50){
-		if ((touchRightStarted && newLeft <= dimMenu && newLeft >= 0) || 
-			(touchLeftStarted && newLeft <= 0 && newLeft >= -dimMenu)) {
-			$.movableview.left = newLeft;
-		}
-		else {
-			// Sometimes newLeft goes beyond its bounds so the view gets stuck.
-			// This is a hack to fix that.
-			if ((touchRightStarted && newLeft < 0) || (touchLeftStarted && newLeft > 0)) {
-				$.movableview.left = 0;
-			}
-			else if (touchRightStarted && newLeft > dimMenu) {
-				$.movableview.left = dimMenu;
-			}
-			else if (touchLeftStarted && newLeft < -dimMenu) {
-				$.movableview.left = -dimMenu;
-			}
-		}
-		if (newLeft > 5 && !touchLeftStarted && !touchRightStarted) {
-			touchRightStarted = true;
-			Ti.App.fireEvent("sliderToggled", {
-				hasSlided : false,
-				direction : "right"
-			});
-		}
-		else if (newLeft < -5 && !touchRightStarted && !touchLeftStarted) {
-			touchLeftStarted = true;
-			Ti.App.fireEvent("sliderToggled", {
-				hasSlided : false,
-				direction : "left"
-			});
-		}
-	}
-	
-});*/
-
+//gestisce l'apertura e chiusura del menu alla pressione del tasto di sinistra 'Menu'
 $.leftButton.addEventListener('touchend', function(e) {
 	if (!touchRightStarted && !touchLeftStarted) {
 		if(buttonPressed==false){
@@ -176,6 +144,7 @@ $.leftButton.addEventListener('touchend', function(e) {
 	}
 });
 
+//gestisce l'apertura e chiusura del menu alla pressione del tasto di destra 'Menu2'
 $.rightButton.addEventListener('touchend', function(e) {
 	if (!touchRightStarted && !touchLeftStarted) {
 		if(buttonPressed==false){
@@ -195,6 +164,7 @@ $.rightButton.addEventListener('touchend', function(e) {
 	}
 });
 
+//gestisce l'apertura verso destra e la chiusura del menu
 exports.toggleLeftSlider = function() {
 	if (!hasSlided) {
 		direction = "right";
@@ -211,6 +181,7 @@ exports.toggleLeftSlider = function() {
 	});
 };
 
+//gestisce l'apertura verso sinistra e la chiusura del menu
 exports.toggleRightSlider = function() {
 	if (!hasSlided) {
 		direction = "left";
@@ -227,17 +198,45 @@ exports.toggleRightSlider = function() {
     });
 };
 
+//gestisce la rotazione del telefono
 exports.handleRotation = function() {
-/*
-  	Add the orientation handler in the controller that loads this widget. Like this:
-	Ti.Gesture.addEventListener('orientationchange', function() {
-		$.handleRotation();
-	});
-*/
 	$.movableview.width = $.navview.width = $.contentview.width = Ti.Platform.displayCaps.platformWidth;
 	$.movableview.height = $.navview.height = $.contentview.height = Ti.Platform.displayCaps.platformHeight;
 };
 
+// Visualizza la vista selezionata nel menu di sinistra
+$.leftTableView.addEventListener('click', function selectRow(e) {
+	rowSelect(e);
+	$.toggleLeftSlider();
+	buttonPressed=false;
+});
+
+// Visualizza la vista selezionata nel menu di sinistra
+$.rightTableView.addEventListener('click', function selectRow(e) {
+	rowSelect(e);
+	$.toggleRightSlider();
+	buttonPressed=false;
+});
+
+//imposta quale menu viene visualizzato e quale nascosto sotto
+Ti.App.addEventListener("sliderToggled", function(e) {
+	if (e.direction == "right") {
+		$.leftMenu.zIndex = 2;
+		$.rightMenu.zIndex = 1;
+	} else if (e.direction == "left") {
+		$.leftMenu.zIndex = 1;
+		$.rightMenu.zIndex = 2;
+	}
+});
+
+//apre e sostituisce la vista corrente con quella selezionata dal menu
+function rowSelect(e) {
+	if (currentView.id != e.row.customView) {
+		$.contentview.remove(currentView);
+		currentView = Alloy.createController(e.row.className).getView();
+		$.contentview.add(currentView);
+	}
+}
 
 
 
@@ -258,13 +257,7 @@ exports.handleRotation = function() {
 
 
 
-
-
-
-
-
-
-
+//creazione del menu
 var leftData = [];
 var rightData = [];
 
@@ -288,41 +281,12 @@ leftData.push(row3);
 $.leftTableView.data = leftData;
 $.rightTableView.data = rightData;
 
-function rowSelect(e) {
-	if (currentView.id != e.row.customView) {
-		$.contentview.remove(currentView);
-		currentView = Alloy.createController(e.row.className).getView();
-		$.contentview.add(currentView);
-	}
-}
-
+//apertura della vista 'nome_page' e impostazione come vista corrente
 var currentView = Alloy.createController("home_page").getView();
 $.contentview.add(currentView);
 
-// Swap views on menu item click
-$.leftTableView.addEventListener('click', function selectRow(e) {
-	rowSelect(e);
-	$.toggleLeftSlider();
-	buttonPressed=false;
-});
-$.rightTableView.addEventListener('click', function selectRow(e) {
-	rowSelect(e);
-	$.toggleRightSlider();
-	buttonPressed=false;
-});
 
-
-
-Ti.App.addEventListener("sliderToggled", function(e) {
-	if (e.direction == "right") {
-		$.leftMenu.zIndex = 2;
-		$.rightMenu.zIndex = 1;
-	} else if (e.direction == "left") {
-		$.leftMenu.zIndex = 1;
-		$.rightMenu.zIndex = 2;
-	}
-});
-
+//apertura  dell'applicazione
 if (Ti.Platform.osname === 'iphone')
 	$.index.open({
 		transition : Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
