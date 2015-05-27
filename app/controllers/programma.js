fa34d095c91343848cf4ee5f71d23009c7992e52
vfurlan <valentino_furlan@yahoo.it>
2015-05-27 15:26:01 +0200

    sistemata pagina programma

--- app/controllers/programma.js
+++ app/controllers/programma.js
@@ -1,19 +1,28 @@
 var args = arguments[0] || {};
 
+var data = new Date();
+var anno = data.getFullYear();
+
 var utenti = Alloy.Collections.instance("utente");
-utenti.fetch({query: 'select * from utente where anno = 2015' });
-alert(utenti.length);
+utenti.fetch({query: 'select * from utente where anno = '+anno });
 
-var buttons = new Array();
-for (var i = 0; i < utenti.length; i++) {
-	buttons[i] = Titanium.UI.createButton({title: utenti.at(i).relatore});
-	buttons[i].addEventListener('click', function(e) {
-       alert(e.source.title);
-    });
-    this.addClass(buttons[i],"button");
-
-	$.programmaContent.add(buttons[i]);
+if(utenti.length>0){
+	var buttons = new Array();
+	for (var i = 0; i < utenti.length; i++) {
+		var relatore = utenti.at(i).get("relatore");
+		relatore = relatore.replace("|", "\n");
+		relatore = relatore.replace("&", " ");
+		relatore = relatore.replace("_", " ");
+		buttons[i] = Titanium.UI.createButton({title: relatore});
+		buttons[i].addEventListener('click', function(e) {
+	       alert(e.source.title);
+	    });
+	    this.addClass(buttons[i],"button");
+	
+		$.programmaContent.add(buttons[i]);
+	}
 }
-
-
+else{
+	$.textLabel.text="Nessun evento disponibile!";
+}
 
