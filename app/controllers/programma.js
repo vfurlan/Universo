bd26022390ce3296ec9e65b8f35008e9f26f06d5
vfurlan <valentino_furlan@yahoo.it>
2015-05-29 18:30:57 +0200

    creata funzione replace

--- app/controllers/programma.js
+++ app/controllers/programma.js
@@ -1,5 +1,15 @@
 var args = arguments[0] || {};
 
+function replace(str){
+	str = str.replace(/\|/g, "\n");
+	str = str.replace(/&/g, " ");
+	str = str.replace(/_/g, " ");
+	str = str.replace(/<br>/g, "\n");
+	str = str.replace(/<p>/g, "");
+	str = str.replace(/<\/p>/g, "");
+	return str;
+}
+
 var data = new Date();
 var anno = data.getFullYear();
 
@@ -10,12 +20,17 @@ if(utenti.length>0){
 	var buttons = new Array();
 	for (var i = 0; i < utenti.length; i++) {
 		var relatore = utenti.at(i).get("relatore");
-		relatore = relatore.replace("|", "\n");
-		relatore = relatore.replace("&", " ");
-		relatore = relatore.replace("_", " ");
-		buttons[i] = Titanium.UI.createButton({title: relatore});
+		var id = utenti.at(i).get("id");
+		relatore = replace(relatore);
+		buttons[i] = Titanium.UI.createButton({title: relatore, titleid:id});
 		buttons[i].addEventListener('click', function(e) {
-	       alert(e.source.title);
+			Ti.App.fireEvent("changeView", {
+				row : {
+					customView: "",
+					className: "evento",
+					idRelatore: e.source.titleid
+				}
+			});
 	    });
 	    this.addClass(buttons[i],"button");
 	
