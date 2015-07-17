f7672eefed6615f2dbb28c1162dad9ea8e25b8f2
vfurlan <valentino_furlan@yahoo.it>
2015-07-17 17:33:20 +0200

    sistemata gestione pdf

--- app/controllers/dove_siamo.js
+++ app/controllers/dove_siamo.js
@@ -1,30 +1,4 @@
 var args = arguments[0] || {};
-/*
-var content='<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d707.3217534201033!2d11.618724!3d44.836088!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477e4e6c83303f17%3A0xd74f5c2162608553!2sPiazza+del+Municipio%2C+44121+Ferrara+FE%2C+Italia!5e0!3m2!1sit!2sit!4v1436549995655" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>';
-
-var mappa = Ti.UI.createWebView({html: content});
-this.addClass(mappa,"mapView");
-$.mapView.add(mappa);
-
-
-*/
-/*
-//Add in the module
-var MapModule = require('ti.map');
-var win = Ti.UI.createWindow({backgroundColor: 'white'});
-var map1 = MapModule.createView({
-    userLocation: true,
-    mapType: MapModule.NORMAL_TYPE,
-    animate: true,
-    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-    height: '50%',
-    top: 0,
-    left: 0,
-    width: '50%'
-});
-$.mapView.add(map1);
-
-*/
 
 var annotation = Alloy.Globals.Map.createAnnotation({
     title : "Piazza del Municipio",
@@ -34,8 +8,6 @@ var annotation = Alloy.Globals.Map.createAnnotation({
     url : "http://web.fe.infn.it/u/gambetti/venerdi/index.php"
 });
 $.mapView.addAnnotation(annotation);
-
-
 
 var recapiti= "I Venerdi Dell'Universo\n"+
 "Sala Estense,\n"+
--- app/controllers/evento.js
+++ app/controllers/evento.js
@@ -2,6 +2,20 @@ var args = arguments[0] || {};
 
 var parser = require('parser');
 
+function showPdf(file){
+	if (Ti.Platform.osname === 'android'){
+		Ti.Android.currentActivity.startActivity(Ti.Android.createIntent({
+			action:Ti.Android.ACTION_VIEW,
+			type:'application/pdf',
+			data:file.getNativePath()}));
+	}
+	else{
+		docViewer = Ti.UI.iOS.createDocumentViewer({url:file.getNativePath()});
+		docViewer.show();
+	}
+}
+
+
 var utenti = Alloy.Collections.instance("utente");
 utenti.fetch({query: 'select * from utente where id = '+args });
 
@@ -27,50 +41,74 @@ if(utenti.length>0){
 	
 	
 	
+	var progressBarCount=new Array();
+	var button=new Array();
+	for(var i=0;i<data.length;i++){
+		progressBarCount[i]=0;
+		button[i]=Titanium.UI.createButton();
+		this.addClass(button[i],"pdfButton");
+		$.pdfButtonView.add(button[i]);
+	}
 	
-	
-	var ind = Titanium.UI.createProgressBar({
-		top:10,
-		width:250,
-		height:'auto',
-		min:0,
-		max:1,
-		value:0,
-		color:'#fff',
-		message: 'Downloading...',
-		font: {fontSize:14, fontWeight:'bold'},
-		style: Titanium.UI.iPhone.ProgressBarStyle
-	});
-	$.multimediaView.add(ind);
-	
-	var xhr = Titanium.Network.createHTTPClient({
-		onload: function() {
-			ind.message="Download complete!";
-			Ti.API.info('PDF downloaded to applicationDataDirectory/Bresadola.pdf');
+	for(var i=0;i<data.length;i++){
+		button[i].addEventListener('click',function(e){
+			//inizializzazione della progress bar relativa al download del pdf
+			var progressBar = Titanium.UI.createProgressBar({
+				top:10,
+				width:250,
+				height:'auto',
+				min:0,
+				max:1,
+				value:0,
+				color:'#fff',
+				message: 'Downloading...',
+				font: {fontSize:14, fontWeight:'bold'},
+				style: Titanium.UI.iPhone.ProgressBarStyle
+			});
 			
+			
+			//inizializzazione del percorso assoluto e nome del file pdf
 			var dir;
 			if (Ti.Platform.osname === 'android')
 				dir=Ti.Filesystem.externalStorageDirectory;
 			else
 				dir=Ti.Filesystem.applicationDataDirectory;
-			file = Ti.Filesystem.getFile(dir,'Bresadola.pdf');
-			file.write(this.responseData); // write to the file
+			var fileName=data[0].cognome+'.pdf';
 			
-			Ti.Android.currentActivity.startActivity(Ti.Android.createIntent({
-				action:Ti.Android.ACTION_VIEW,
-				type:'application/pdf',
-				data:file.getNativePath()}));
-			Ti.API.info(file.getNativePath());
-		},
-		timeout: 10000
-	});
-	xhr.open('GET','http://www.fe.infn.it/venerdi/VENERDIHOME_file/pdf2015/Bresadola.pdf');
-	xhr.send();
-	xhr.ondatastream = function(e) {
-		ind.value = e.progress ;
-		Ti.API.info('ONDATASTREAM - PROGRESS: ' + e.progress);
-	};
-	
+			file = Ti.Filesystem.getFile(dir,fileName);
+			if(file.exists()){
+				showPdf(file);
+			}
+			else{
+				if(progressBarCount[0]<1){
+					progressBarCount[0]=1;
+					$.pdfView.add(progressBar);
+					var xhr = Titanium.Network.createHTTPClient({
+						onload: function() {
+							progressBar.message="Download complete!";
+							
+							file = Ti.Filesystem.getFile(dir,fileName);
+							file.write(this.responseData); // write to the file
+							
+							showPdf(file);
+						},
+						timeout: 10000
+					});
+					xhr.open('GET','http://www.fe.infn.it/venerdi/VENERDIHOME_file/pdf'+model.get("anno")+'/'+fileName);
+					xhr.send();
+					xhr.ondatastream = function(e) {
+						progressBar.value = e.progress ;
+						Ti.API.info('ONDATASTREAM - PROGRESS: ' + e.progress);
+					};
+				}
+			}
+			
+			
+			
+			
+			
+		});
+	}
 	
 	
 	
@@ -80,7 +118,17 @@ if(utenti.length>0){
 	var content = "<div align='center'><iframe src='"+model.get("youtube")+"' frameborder='0' allowfullscreen></iframe></div>";
 	var video = Ti.UI.createWebView({html: content});
 	this.addClass(video,"youtubeView");
-	$.multimediaView.add(video);
+	$.videoView.add(video);
+	
+	
+	
+	
+	/*
+	var youtubePlayer = require('titutorial.youtubeplayer');
+	var playVideo = Ti.UI.createButton({ title : 'Play video', height : '40dp', width : Ti.UI.SIZE, top : '100dp' }); 
+	$.multimediaView.add(playVideo);
+	playVideo.addEventListener('click', function() { youtubePlayer.playVideo("JwowlWH0ZRk"); });
+	*/
 	
 	
 	//Ti.Platform.openURL('http://www.youtube.com/embed/US7xaxyFETI?rel=0'); // use this to play the video
