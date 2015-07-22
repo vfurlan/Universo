62ced9c54ce1bafd1b7e2b3a74bee4194136a1e4
vfurlan <valentino_furlan@yahoo.it>
2015-07-22 17:29:09 +0200

    sistemata view contatti

--- app/controllers/contatti.js
+++ app/controllers/contatti.js
@@ -1,1 +1,44 @@
 var args = arguments[0] || {};
+
+
+var organizzazione1= "Curatore Responsabile Prof. Mauro Savrie'\n"+
+"Email: savrie@fe.infn.it\n"+
+"Telefono: 0532974268";
+
+var organizzazione2= "Organizzazione Michele Parise\n"+
+"Email: parise@fe.infn.it\n"+
+"Telefono: 05329742098";
+
+var organizzazione3= "Assistenza Tecnica Gloriano Bighi\n"+
+"Email: gloriano.bighi@unife.it\n"+
+"Telefono: 05329742907";
+
+var organizzazione4= "Segreteria Amministrativa Dott.ssa Paola Fabbri\n"+
+"Email: paola@fe.infn.it\n"+
+"Telefono: 05329742280";
+
+$.textOrganizzazioneLabel1.text=organizzazione1;
+$.textOrganizzazioneLabel2.text=organizzazione2;
+$.textOrganizzazioneLabel3.text=organizzazione3;
+$.textOrganizzazioneLabel4.text=organizzazione4;
+
+
+var informazioni1= "Dipartimento di Fisica e Scienze della Terra\n"+
+"Via G. Saragat, 1 - Blocco C - 44122 Ferrara\n"+
+"WWW: http://fst.unife.it\n"+
+"Email: universo@unife.it";
+
+var informazioni2= "INFN - Sezione di Ferrara\n"+
+"Via G. Saragat, 1 - Blocco C - 44122 Ferrara\n"+
+"WWW: http://www.fe.infn.it";
+
+var informazioni3= "Gruppo Astrofili Columbia\n"+
+"Via Argine Mezzano,1 - 44020 Ostellato(FE)\n"+
+"Ferruccio Zanotti\n"+
+"Massimiliano Di Giuseppe\n"+
+"E-mail: esploriamoluniverso@gmail.com\n"+
+"WWW: http://www.esploriamoluniverso.com";
+
+$.textInformazioniLabel1.text=informazioni1;
+$.textInformazioniLabel2.text=informazioni2;
+$.textInformazioniLabel3.text=informazioni3;
