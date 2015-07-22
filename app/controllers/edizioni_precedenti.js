4513748d45189d875065a983e5c242b5b235ce3c
vfurlan <valentino_furlan@yahoo.it>
2015-07-22 17:30:44 +0200

    sistemata view edizioni precedenti

--- app/controllers/edizioni_precedenti.js
+++ app/controllers/edizioni_precedenti.js
@@ -1,1 +1,31 @@
 var args = arguments[0] || {};
+
+var date = new Date();
+var currentYear = date.getFullYear();
+
+var utenti = Alloy.Collections.instance("utente");
+utenti.fetch({query: 'select distinct anno from utente where anno < '+currentYear });
+
+if(utenti.length>0){
+	var buttons = new Array();
+	for (var i = 0; i < utenti.length; i++) {
+		var year = utenti.at(i).get("anno");
+		buttons[i] = Titanium.UI.createButton({title: year, titleid: year});
+		buttons[i].addEventListener('click', function(e) {
+			Ti.App.fireEvent("changeView", {
+				row : {
+					customView: "",
+					className: "relatori",
+					value: e.source.titleid
+				}
+			});
+	    });
+	    this.addClass(buttons[i],"button");
+	
+		$.buttonsView.add(buttons[i]);
+	}
+}
+else{
+	$.textLabel.text="Nessuna edizione precedente!";
+}
+
